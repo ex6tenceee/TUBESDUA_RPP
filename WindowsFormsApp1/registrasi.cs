1caad4258cfc0d7e079d0f6d73ef868e07abf5a5
abdulghofur12 <ghofursans12@gmail.com>
2022-07-03 22:17:52 +0700

    menambahkan variable

--- WindowsFormsApp1/registrasi.cs
+++ WindowsFormsApp1/registrasi.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama, telp, alamat, username, password;
+
+
+        }
     }
 }
