4a36be9532fbc5778d2258cf47b78f6f7a26427c
abdulghofur12 <ghofursans12@gmail.com>
2022-07-03 22:20:52 +0700

    menambahkan tenik defensive

--- WindowsFormsApp1/registrasi.cs
+++ WindowsFormsApp1/registrasi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
@@ -26,6 +27,12 @@ namespace WindowsFormsApp1
             alamat = textBox3.Text;
             username = textBox4.Text;
             password = textBox5.Text;
+
+            Debug.Assert(nama.Length > 0, "nama tidak boleh kosong");
+            Debug.Assert(telp.Length > 0 && telp.Length < 13, "no tidak boleh kosong dan tidak boleh lebih dari 13");
+            Debug.Assert(alamat.Length > 0, "alamat tidak boleh kosong");
+            Debug.Assert(username.Length > 0 && username.Length < 11, "username tidak boleh lebih dari 11");
+            Debug.Assert(password.Length > 0 && password.Length < 10, "password tidak boleh lebih dari 11");
 
         }
     }
